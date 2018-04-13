995212ac70294cf9170e9b089469dd65534e977f
agustinperrotta_31@hotmail.com <peragu>
2018-04-13 19:10:25 -0300

    commerces list working

--- public/javascripts/controllers/commerces.js
+++ public/javascripts/controllers/commerces.js
@@ -2,10 +2,15 @@
 hoyComoApp.controller('commercesCtrl', function ($scope, $http, $window, $rootScope, toastr) {
     $scope.commerces = [];
 
-    $http.get("/api/v1/commerces")
-        .success(function(data){
-            $scope.commerces = data;
-        }).error(function(err){
-            toastr.error(err.message);
-        });
+    $http({
+        url: "/api/v1/commerces",
+        method: "GET",
+        headers: {
+            'authorization' : $rootScope.auth
+        }
+    }).success(function(data, status, headers, config){
+        $scope.commerces = data;
+    }).error(function(err){
+        toastr.error(err.message);
+    });
 });
