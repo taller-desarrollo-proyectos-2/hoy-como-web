bbd7b8b0011d0b3755c13a0e8d65b09c968b7720
agustinperrotta_31@hotmail.com <peragu>
2018-04-13 13:35:34 -0300

    dash generico

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -1,6 +1,19 @@
 hoyComoApp.controller('mainCtrl', function ($scope, $http, $window) {
     
+    
+    $scope.menu = [{showName: 'Comercios', route: '/login', icon: 'ti-panel'}, {showName: 'la vida', route: '/lavida', icon: 'ti-rocket'}];
+        
+        
     $scope.content = "/dash";
+    
+    $scope.setContent = function(route){
+        $scope.dashContent = route;
+    };
+
+    $scope.isActive = function(route){
+        if($scope.dashContent === route) return true;
+        return false;
+    };
 //    $http.get("ruta para saber si estoy logueado")
 //        .success(function(data) { 
 //            $scope.content = "/dash";
