0d59f3211ae6b3519af40166d3906bb4dc1a03ce
agustinperrotta_31@hotmail.com <peragu>
2018-04-13 14:17:14 -0300

    login screen done moving within login and dash not working :/

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -4,7 +4,7 @@ hoyComoApp.controller('mainCtrl', function ($scope, $http, $window) {
     $scope.menu = [{showName: 'Comercios', route: '/login', icon: 'ti-panel'}, {showName: 'la vida', route: '/lavida', icon: 'ti-rocket'}];
         
         
-    $scope.content = "/dash";
+    $scope.content = "/login";
     
     $scope.setContent = function(route){
         $scope.dashContent = route;
@@ -14,11 +14,9 @@ hoyComoApp.controller('mainCtrl', function ($scope, $http, $window) {
         if($scope.dashContent === route) return true;
         return false;
     };
-//    $http.get("ruta para saber si estoy logueado")
-//        .success(function(data) { 
-//            $scope.content = "/dash";
-//        }).error(function(){
-//            $scope.content = "/login";
-//        });
+    
+    $scope.login = function (){
+        $scope.content = "/login";
+    };
     
 });
