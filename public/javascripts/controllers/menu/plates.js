a0214513d73fe475a3db7b19737da3bcf5c6680c
agustinperrotta_31@hotmail.com <peragu>
2018-04-19 15:44:12 -0300

    adding optional to plate optionals working. optionals dialog in plates screen

--- public/javascripts/controllers/menu/plates.js
+++ public/javascripts/controllers/menu/plates.js
@@ -4,9 +4,13 @@ hoyComoApp.controller('platesCtrl', function ($scope, $http, $window, $rootScope
     $scope.plates = [];
     $scope.currentPlate = {};
     $scope.categories = [];
+    $scope.editModal = true;
+    $scope.selected = {};
+
 
     indexPlates();
     indexCategories();
+    indexOptionals();
 
     function indexPlates(){
         $http({
@@ -17,6 +21,20 @@ hoyComoApp.controller('platesCtrl', function ($scope, $http, $window, $rootScope
             }
         }).success(function(data, status, headers, config){
             $scope.plates = data;
+        }).error(function(err){
+            toastr.error(err.message);
+        });
+    }
+
+    function indexOptionals(){
+        $http({
+            url: "/api/v1/optionals",
+            method: "GET",
+            headers: {
+                'authorization' : $rootScope.auth
+            }
+        }).success(function(data, status, headers, config){
+            $scope.optionals = data;
         }).error(function(err){
             toastr.error(err.message);
         });
@@ -37,19 +55,21 @@ hoyComoApp.controller('platesCtrl', function ($scope, $http, $window, $rootScope
     }
 
     $scope.toggleCreateModal = function() {
+        if($scope.editModal) $scope.currentPlate = {optionals : []};
         $scope.editModal = false;
         $("#platesModal").modal("toggle");
     };
 
-    $scope.toggleCategoryCreationModal = function() {
+    $scope.toggleInCreationModal = function(modal) {
         $scope.currentCategory = {};
         $("#platesModal").modal("toggle");
-        $("#categoriesModal").modal("toggle");
+        $(modal).modal("toggle");
     };
 
-    $scope.backToPlatesModal = function() {
+    $scope.backToPlatesModal = function(modal) {
         $scope.currentCategory = {};
-        $("#categoriesModal").modal("toggle");
+        $scope.currentOptional = {};
+        $(modal).modal("toggle");
         $("#platesModal").modal("toggle");
     };
 
@@ -93,8 +113,7 @@ hoyComoApp.controller('platesCtrl', function ($scope, $http, $window, $rootScope
         }
     };
     
-    function createFinish(e)
-    { 
+    function createFinish(e){ 
         if(this.status === 200){
             $('#plateForm').get(0).reset();
             toastr.success("Plato creado con exito.");
@@ -121,8 +140,7 @@ hoyComoApp.controller('platesCtrl', function ($scope, $http, $window, $rootScope
         xhr.send(formData);
     };
     
-    function updateFinish(e)
-    { 
+    function updateFinish(e){ 
         if(this.status === 200){
             $('#plateForm').get(0).reset();
             toastr.success("Plato creado con exito.");
@@ -132,6 +150,26 @@ hoyComoApp.controller('platesCtrl', function ($scope, $http, $window, $rootScope
         } else {
             toastr.error("No se pudo crear el plato.");
         }
+    }
+
+    $scope.addOptional = function (optional){
+        if(!optionalOnList(optional)){
+            $scope.currentPlate.optionals.push(optional);
+        } else {
+            toastr.error("El opcional ya se encuentra agregado.");
+        }
+        $scope.selected.optional = undefined;
+    };
+
+    $scope.removeOptional = function (index){
+        $scope.currentPlate.optionals.splice(index, 1);
+    };
+
+    function optionalOnList(optional){
+        for(var option of $scope.currentPlate.optionals){
+            if(option.id == optional.id) return true;
+        }
+        return false;
     }
 
 
