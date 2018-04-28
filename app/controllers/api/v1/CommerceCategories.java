bcc4fe88449b980dbae860ed7679096bf2d56ae9
fcaldora <facundo.caldora@gmail.com>
2018-04-28 17:56:11 -0300

    [US-19]Agrego ruta de listado de categorias de comercios

--- app/controllers/api/v1/CommerceCategories.java
+++ app/controllers/api/v1/CommerceCategories.java
@@ -1,5 +1,6 @@
 package controllers.api.v1;
 
+import annotations.Authenticate;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import models.CommerceCategory;
 import play.Logger;
@@ -11,6 +12,7 @@ public class CommerceCategories extends Controller {
 
     private static Logger.ALogger logger = Logger.of("commerce-categories");
 
+    @Authenticate(types = {"BACKOFFICE"})
     public static Result list(){
         try{
             return ok(SerializerService.serializeList(CommerceCategory.findAll()));
