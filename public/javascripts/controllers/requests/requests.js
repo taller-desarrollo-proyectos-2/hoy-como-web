732ae51544bbaddf408e354302976b2fde35f385
agustinperrotta_31@hotmail.com <peragu>
2018-05-03 20:17:51 -0300

    integration should work

--- public/javascripts/controllers/requests/requests.js
+++ public/javascripts/controllers/requests/requests.js
@@ -1,6 +1,19 @@
 
 hoyComoApp.controller('requestsCtrl', function ($scope, $http, $window, $rootScope, toastr, $filter, $uibModal) {
 
+    index();
+
+    function index(){
+        $http({
+            url: "/api/v1/requests",
+            method: "GET"
+        }).success(function(data, status, headers, config){
+            $scope.requests = data;
+        }).error(function(err){
+            toastr.error(err.message);
+        });
+    }
+
     $scope.openUserDetailsModal = (user) =>{
         const modalInstance = $uibModal.open({
             animation: true,
