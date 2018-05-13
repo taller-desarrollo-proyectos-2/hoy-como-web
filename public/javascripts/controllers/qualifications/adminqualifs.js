8484245d952c4fff789b920f73f52ac5660a68e1
agustinperrotta_31@hotmail.com <peragu>
2018-05-13 19:43:11 -0300

    qualifs screen

--- public/javascripts/controllers/qualifications/adminqualifs.js
+++ public/javascripts/controllers/qualifications/adminqualifs.js
@@ -11,6 +11,22 @@ hoyComoApp.controller('qualifCtrl', function ($scope, $http, $window, $rootScope
                                 username: "Juan",
                                 comment: "Un desastre",
                                 replica: "Lamentamos lo ocurrido."
+                            }, {
+                                date: "19/3/18 14:00hs",
+                                rating: 5,
+                                username: "Roberto",
+                                comment: "Perfecto como siempre!",
+                                replica: "Esperamos tu pedido nuevamente."
+                            }, {
+                                date: "19/3/18 8:00hs",
+                                rating: 3,
+                                username: "Jorge",
+                                comment: "Todo bien.."
+                            }, {
+                                date: "17/3/18 12:00hs",
+                                rating: 1,
+                                username: "Micaela",
+                                comment: "Nunca más."
                             }];
 
 });
