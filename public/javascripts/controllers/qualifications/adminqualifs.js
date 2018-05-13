8e439d0242a1a5bd6e186c75963777c176a7cc68
agustinperrotta_31@hotmail.com <peragu>
2018-05-13 19:20:26 -0300

    stars ready for showing qualification's rating

--- public/javascripts/controllers/qualifications/adminqualifs.js
+++ public/javascripts/controllers/qualifications/adminqualifs.js
@@ -1,12 +1,12 @@
 hoyComoApp.controller('qualifCtrl', function ($scope, $http, $window, $rootScope, toastr) {
 
     $scope.qualifications = [{
-                                date: "20/318 19:00hs",
+                                date: "20/3/18 21:30hs",
                                 rating: 4,
                                 username: "Agus",
                                 comment: "Muy buena la comida."
                             }, {
-                                date: "20/318 19:00hs",
+                                date: "20/3/18 19:00hs",
                                 rating: 2,
                                 username: "Juan",
                                 comment: "Un desastre",
