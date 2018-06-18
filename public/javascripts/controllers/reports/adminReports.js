bcc5528ed1abbe4435956adffea007472f7bdbc2
agustinperrotta_31@hotmail.com <peragu>
2018-06-17 23:04:17 -0300

    admin report working

--- public/javascripts/controllers/reports/adminReports.js
+++ public/javascripts/controllers/reports/adminReports.js
@@ -1,6 +1,9 @@
 hoyComoApp.controller('reportsCtrl', function ($scope, $http, $window, $rootScope, toastr, $interval, $uibModal) {
 
     $scope.data = {};
+    $scope.data.from = new Date();
+    $scope.data.from.setMonth($scope.data.from.getMonth()-1);
+    $scope.data.to = new Date();
     $scope.validPeriodSelected = false;
 
     $scope.checkPeriod = () =>{
@@ -9,8 +12,11 @@ hoyComoApp.controller('reportsCtrl', function ($scope, $http, $window, $rootScop
         if($scope.data.from && $scope.data.to){
             if ($scope.data.to < $scope.data.from) {
                 toastr.error("Revise las fechas ingresadas"); 
-            } else {
+            } else if (dateDiffInDays() > 31){
+                toastr.error("El periodo seleccionado no puede ser mayor a 31 dias."); 
+            }else {
                 $scope.validPeriodSelected = true;
+                getData();
             }
         }
     };
@@ -19,5 +25,22 @@ hoyComoApp.controller('reportsCtrl', function ($scope, $http, $window, $rootScop
         $scope.validPeriodSelected = false;
     };
 
+    function dateDiffInDays () {
+        var diff =($scope.data.to.getTime() - $scope.data.from.getTime()) / 1000;
+        diff /= (60 * 60 * 24);
+        return Math.abs(Math.round(diff));
+    }
 
+    function getData(){
+        var from = $scope.data.from.toISOString().split("T")[0];
+        var to = $scope.data.to.toISOString().split("T")[0];
+        $http({
+            url: "/api/v1/reports?from=" + from + "&to=" + to,
+            method: "GET"
+        }).success(function(data, status, headers, config){
+            $scope.reports = data;
+        }).error(function(err){
+            toastr.error(err.message);
+        });
+    }
 });
